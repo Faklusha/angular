429da551a9a033e1fe65fb92a0eab19c9bb9e148
GRODNO\Yuliana_Sukonka <Yuliana_Sukonka@epam.com>
2018-08-08 14:38:13 +0300

    mock-back was added

---
+++ mock-back/services/core/courses/courses.middleware.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const url = require('url');
+
+module.exports = (server) => {
+
+	router.get('/courses', (req, res, next) => {
+		let url_parts = url.parse(req.originalUrl, true),
+			query = url_parts.query,
+			from = query.start || 0,
+			to = +query.start + +query.count,
+			sort = query.sort,
+			queryStr = query.query,
+			courses = server.db.getState().courses;
+
+			if (!!query.textFragment) {
+				courses = courses.filter((course) => course.name.concat(course.description).toUpperCase().indexOf(query.textFragment.toUpperCase()) >= 0);
+			}
+
+		if (courses.length < to || !to) {
+			to = courses.length;
+		}
+		courses = courses.slice(from, to);
+
+		res.json(courses);
+	});
+
+    router.post('/courses/:id', (req, res, next) => {
+    	const newCourse = req.body.course;
+    courses = server.db.getState().courses;
+
+     courses.push(newCourse);
+     res.json(courses);
+});
+
+	return router;
+};
